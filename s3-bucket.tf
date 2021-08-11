ac8bb2eac6edfd761b92bd37a48fae937ca24c90
sasidhar <sasidharpaws@gmail.com>
2021-08-11 19:41:55 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
-module "s3-bucket_example_complete" {
-  source  = "app.terraform.io/sasidhar-training/s3-bucket/aws//examples/complete"
+module "s3-bucket" {
+  source  = "app.terraform.io/sasidhar-training/s3-bucket/aws"
   version = "2.2.0"
+  # insert required variables here
   bucket_prefix = "test"
-  }
+}
